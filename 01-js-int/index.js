17fd889d18e363eb8c4866f6674433028f79550b
utsav kalathiya <ukalathiya54@gmail.com>
2025-08-07 13:22:08 +0530

    intro

---
+++ 01-js-int/index.js
@@ -0,0 +1,38 @@
+// const utsav = document.getElementById("utsav")
+
+// chapter 2ps
+// problem Number 1
+
+// let age =prompt("whot is your age?")
+// if( age>10 && age<20){
+//     console.log(" your age lies between 10 and 20")
+// }
+// else{
+//       console.log(" your age  doesnt lies between 10 and 20")
+// }
+
+
+// problem Number 2
+
+let age =prompt("whot is your age?")
+switch(age){
+    case '12':
+        console.log("your age is 12")
+        break
+
+    case '13':
+        console.log("your age is 13")
+        break
+        
+    case '14':
+        console.log("your age is 14")
+        break
+
+    case '15':
+        console.log("your age is 15")
+        break
+
+    default:
+        console.log("your age is not special")
+}
+
